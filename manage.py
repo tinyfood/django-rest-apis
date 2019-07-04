97da793016b61cb974a69d55b2297ae959e4f0ab
G. Willson <greg@biomassiv.es>
2019-07-04 13:28:47 -0600

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,14 @@
 import os
 import sys
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ STATIC_URL = '/static/'
+ STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
